56270a338afa3bc65697ee415dd0e1de62c416b8
Savagecoder77 <savagecoder77@gmail.com>
2018-12-01 13:37:52 -0800

    create functions

--- commiter.py
+++ commiter.py
@@ -1,2 +1,15 @@
 import requests
 import time
+import schedule
+
+def checkGitHub():
+    pass
+
+def job():
+    pass
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
+schedule.every().day.at("9:30").do(run_threaded, job)
